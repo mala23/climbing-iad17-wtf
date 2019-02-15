eee23d2005ec628e50482311550e3872018d159c
Colin Nico Schmid <mail@mala23.me>
2019-02-15 13:21:33 +0100

    start out creating the hatefulsix component

--- climbing-iad17-wtf/src/pages/index.js
+++ climbing-iad17-wtf/src/pages/index.js
@@ -4,12 +4,14 @@ import { Link } from "gatsby"
 import Layout from "../components/layout"
 import Intro from "../components/intro"
 import Image from "../components/image"
+import HatefulSix from "../components/hatefulsix"
 import SEO from "../components/seo"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="How Sport Climbing Works" keywords={[`climbing`, `sports`, `sport climbing`, `olympic`]} />
     <Intro />
+    <HatefulSix />
   </Layout>
 )
 
